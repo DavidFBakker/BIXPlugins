eb9fcb525ffae881da91abbd940fb3510468cdf5
DavidFBakker <dbakker@arrayasolutions.com>
2016-12-24 13:18:41 -0500

    Add lifxnet in with modifications

---
+++ LifxNet/LifxClient.Discovery.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+
+namespace LifxNet
+{
+	public partial class LifxClient : IDisposable
+	{
+		private static Random randomizer = new Random();
+		private UInt32 discoverSourceID;
+		private CancellationTokenSource _DiscoverCancellationSource;
+		private Dictionary<string, Device> DiscoveredBulbs = new Dictionary<string, Device>();
+
+		/// <summary>
+		/// Event fired when a LIFX bulb is discovered on the network
+		/// </summary>
+		public event EventHandler<DeviceDiscoveryEventArgs> DeviceDiscovered;
+		/// <summary>
+		/// Event fired when a LIFX bulb hasn't been seen on the network for a while (for more than 5 minutes)
+		/// </summary>
+		public event EventHandler<DeviceDiscoveryEventArgs> DeviceLost;
+
+		private IList<Device> devices = new List<Device>();
+		
+		/// <summary>
+		/// Gets a list of currently known devices
+		/// </summary>
+		public IEnumerable<Device> Devices { get { return devices; } }
+
+		/// <summary>
+		/// Event args for <see cref="DeviceDiscovered"/> and <see cref="DeviceLost"/> events.
+		/// </summary>
+		public sealed class DeviceDiscoveryEventArgs : EventArgs
+		{
+			/// <summary>
+			/// The device the event relates to
+			/// </summary>
+			public Device Device { get; internal set; }
+		}
+
+		private async void ProcessDeviceDiscoveryMessage(HostName remoteAddress, string remotePort, LifxResponse msg)
+		{
+			if (DiscoveredBulbs.ContainsKey(remoteAddress.ToString()))  //already discovered
+            {
+				DiscoveredBulbs[remoteAddress.ToString()].LastSeen = DateTime.UtcNow; //Update datestamp
+				return;
+			}
+			if (msg.Source != discoverSourceID || //did we request the discovery?
+				_DiscoverCancellationSource == null ||
+				_DiscoverCancellationSource.IsCancellationRequested) //did we cancel discovery?
+				return;
+
+			var device = new LightBulb()
+			{
+				HostName = remoteAddress,
+				Service = msg.Payload[0],
+				Port = BitConverter.ToUInt32(msg.Payload, 1),
+				LastSeen = DateTime.UtcNow
+        };
+		    device.State = await GetLightStateAsync(device);
+
+			DiscoveredBulbs[remoteAddress.ToString()] = device;
+
+            device.State= await GetLightStateAsync(device);
+		   // device.IsOn = await GetLightPowerAsync(device);
+
+            devices.Add(device);
+			if (DeviceDiscovered != null)
+			{
+				DeviceDiscovered(this, new DeviceDiscoveryEventArgs() { Device = device });
+			}
+		}
+
+		/// <summary>
+		/// Begins searching for bulbs.
+		/// </summary>
+		/// <seealso cref="DeviceDiscovered"/>
+		/// <seealso cref="DeviceLost"/>
+		/// <seealso cref="StopDeviceDiscovery"/>
+		public void StartDeviceDiscovery()
+		{
+			if (_DiscoverCancellationSource != null && !_DiscoverCancellationSource.IsCancellationRequested)
+				return;
+			_DiscoverCancellationSource = new CancellationTokenSource();
+			var token = _DiscoverCancellationSource.Token;
+			var source = discoverSourceID = (uint)randomizer.Next(int.MaxValue);
+			//Start discovery thread
+            Task.Run(async () =>
+			{
+				System.Diagnostics.Debug.WriteLine("Sending GetServices");
+				FrameHeader header = new FrameHeader()
+				{
+					Identifier = source
+				};
+				while (!token.IsCancellationRequested)
+				{
+					try
+					{
+						await BroadcastMessageAsync<UnknownResponse>(null, header, MessageType.DeviceGetService, null);
+					}
+					catch { }
+					await Task.Delay(5000);
+					var lostDevices = devices.Where(d => (DateTime.UtcNow - d.LastSeen).TotalMinutes > 5).ToArray();
+					if(lostDevices.Any())
+					{
+						foreach(var device in lostDevices)
+						{
+							devices.Remove(device);
+							DiscoveredBulbs.Remove(device.HostName.ToString());
+							if (DeviceLost != null)
+								DeviceLost(this, new DeviceDiscoveryEventArgs() { Device = device });
+						}
+					}
+				}
+			});
+		}
+
+		/// <summary>
+		/// Stops device discovery
+		/// </summary>
+		/// <seealso cref="StartDeviceDiscovery"/>
+		public void StopDeviceDiscovery()
+		{
+			if (_DiscoverCancellationSource == null || _DiscoverCancellationSource.IsCancellationRequested)
+				return;
+			_DiscoverCancellationSource.Cancel();
+			_DiscoverCancellationSource = null;
+		}
+	}
+
+	/// <summary>
+	/// LIFX Generic Device
+	/// </summary>
+	public abstract class Device
+	{
+		internal Device() { }
+		/// <summary>
+		/// Hostname for the device
+		/// </summary>
+		public HostName HostName { get; internal set; }
+
+	    public string HostNameString
+	    {
+	        get
+	        {
+	            return HostName.ToString();
+	            
+	        } 
+	        
+	    }
+
+	    /// <summary>
+        /// Service ID
+        /// </summary>
+        public byte Service { get; internal set; }
+		/// <summary>
+		/// Service port
+		/// </summary>
+		public UInt32 Port { get; internal set; }
+
+	    public bool IsOn { get; set; }
+
+	    public LightStateResponse State { get; set; }
+        public DateTime LastSeen { get; set; }
+	}
+	/// <summary>
+	/// LIFX light bulb
+	/// </summary>
+	public sealed class LightBulb : Device
+	{
+		internal LightBulb()
+		{
+		}
+	}
+}
---
+++ LifxNet/LifxClient.LightOperations.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+
+namespace LifxNet
+{
+	public partial class LifxClient : IDisposable
+	{
+		private Dictionary<UInt32, Action<LifxResponse>> taskCompletions = new Dictionary<uint, Action<LifxResponse>>();
+
+		/// <summary>
+		/// Turns a bulb on using the provided transition time
+		/// </summary>
+		/// <param name="bulb"></param>
+		/// <param name="transitionDuration"></param>
+		/// <returns></returns>
+		public Task TurnBulbOnAsync(LightBulb bulb, TimeSpan transitionDuration)
+		{
+			System.Diagnostics.Debug.WriteLine("Sending TurnBulbOn to {0}", bulb.HostName);
+			return SetLightPowerAsync(bulb, transitionDuration, true);
+		}
+		/// <summary>
+		/// Turns a bulb off using the provided transition time
+		/// </summary>
+		public Task TurnBulbOffAsync(LightBulb bulb, TimeSpan transitionDuration)
+		{
+			System.Diagnostics.Debug.WriteLine("Sending TurnBulbOff to {0}", bulb.HostName);
+			return SetLightPowerAsync(bulb, transitionDuration, false);
+		}
+		private async Task SetLightPowerAsync(LightBulb bulb, TimeSpan transitionDuration, bool isOn)
+		{
+			if (bulb == null)
+				throw new ArgumentNullException("bulb");
+			if (transitionDuration.TotalMilliseconds > UInt32.MaxValue ||
+				transitionDuration.Ticks < 0)
+				throw new ArgumentOutOfRangeException("transitionDuration");
+
+			FrameHeader header = new FrameHeader()
+			{
+				Identifier = (uint)randomizer.Next(),
+				AcknowledgeRequired = true
+			};
+
+			var b = BitConverter.GetBytes((UInt16)transitionDuration.TotalMilliseconds);
+		    bulb.IsOn = isOn;
+		    bulb.State.IsOn = isOn;
+			await BroadcastMessageAsync<AcknowledgementResponse>(bulb.HostName, header, MessageType.LightSetPower,
+				(UInt16)(isOn ? 65535 : 0), b
+			).ConfigureAwait(false);
+		}
+		/// <summary>
+		/// Gets the current power state for a light bulb
+		/// </summary>
+		/// <param name="bulb"></param>
+		/// <returns></returns>
+		public async Task<bool> GetLightPowerAsync(LightBulb bulb)
+		{
+			FrameHeader header = new FrameHeader()
+			{
+				Identifier = (uint)randomizer.Next(),
+				AcknowledgeRequired = true
+			};
+			return (await BroadcastMessageAsync<LightPowerResponse>(
+				bulb.HostName, header, MessageType.LightGetPower).ConfigureAwait(false)).IsOn;
+		}
+
+		/// <summary>
+		/// Sets color and temperature for a bulb
+		/// </summary>
+		/// <param name="bulb"></param>
+		/// <param name="color"></param>
+		/// <param name="kelvin"></param>
+		/// <returns></returns>
+		public Task SetColorAsync(LightBulb bulb, Windows.UI.Color color, UInt16 kelvin)
+		{
+			return SetColorAsync(bulb, color, kelvin, TimeSpan.Zero);
+		}
+		/// <summary>
+		/// Sets color and temperature for a bulb and uses a transition time to the provided state
+		/// </summary>
+		/// <param name="bulb"></param>
+		/// <param name="color"></param>
+		/// <param name="kelvin"></param>
+		/// <param name="transitionDuration"></param>
+		/// <returns></returns>
+		public Task SetColorAsync(LightBulb bulb, Windows.UI.Color color, UInt16 kelvin, TimeSpan transitionDuration)
+		{
+			var hsl = Utilities.RgbToHsl(color);
+			return SetColorAsync(bulb, hsl[0], hsl[1], hsl[2], kelvin, transitionDuration);
+		}
+
+        /// <summary>
+		/// Sets brightness for a bulb and uses a transition time to the provided state
+		/// </summary>
+		/// <param name="bulb"></param>
+		/// <param name="color"></param>
+		/// <param name="kelvin"></param>
+		/// <param name="transitionDuration"></param>
+		/// <returns></returns>
+		public Task SetBrightnesAsync(LightBulb bulb, double brightnessPercent, TimeSpan transitionDuration)
+        {
+            var dim = (ushort)(65535 * (brightnessPercent / 100.0));
+            var hue = bulb.State.Hue;
+            var saturation = bulb.State.Saturation;
+
+            return SetColorAsync(bulb, hue, saturation,dim, 0, transitionDuration);
+        }
+
+        /// <summary>
+        /// Sets color and temperature for a bulb and uses a transition time to the provided state
+        /// </summary>
+        /// <param name="bulb">Light bulb</param>
+        /// <param name="hue">0..65535</param>
+        /// <param name="saturation">0..65535</param>
+        /// <param name="brightness">0..65535</param>
+        /// <param name="kelvin">2500..9000</param>
+        /// <param name="transitionDuration"></param>
+        /// <returns></returns>
+        public async Task SetColorAsync(LightBulb bulb,
+			UInt16 hue,
+			UInt16 saturation,
+			UInt16 brightness,
+			UInt16 kelvin,
+			TimeSpan transitionDuration)
+		{
+			if (transitionDuration.TotalMilliseconds > UInt32.MaxValue ||
+				transitionDuration.Ticks < 0)
+				throw new ArgumentOutOfRangeException("transitionDuration");
+			if (kelvin!=0 && ( kelvin < 2500 || kelvin > 9000))
+			{
+				throw new ArgumentOutOfRangeException("kelvin", "Kelvin must be between 2500 and 9000");
+			}
+
+				System.Diagnostics.Debug.WriteLine("Setting color to {0}", bulb.HostName);
+			FrameHeader header = new FrameHeader()
+			{
+				Identifier = (uint)randomizer.Next(),
+				AcknowledgeRequired = true
+			};
+			UInt32 duration = (UInt32)transitionDuration.TotalMilliseconds;
+			var durationBytes = BitConverter.GetBytes(duration);
+            //var h = BitConverter.GetBytes(hue);
+            //var s = BitConverter.GetBytes(saturation);
+            //var b = BitConverter.GetBytes(brightness);
+            //var k = BitConverter.GetBytes(kelvin);
+
+            bulb.State.Hue = hue;
+            bulb.State.Saturation = saturation;
+            bulb.State.Brightness = brightness;
+            bulb.State.Kelvin = kelvin;
+
+            await BroadcastMessageAsync<AcknowledgementResponse>(bulb.HostName, header,
+				MessageType.LightSetColor, (byte)0x00, //reserved
+					hue, saturation, brightness, kelvin, //HSBK
+					duration
+			);
+		}
+
+
+        //public async Task SetBrightnessAsync(LightBulb bulb,
+        //    UInt16 brightness,
+        //    TimeSpan transitionDuration)
+        //{
+        //    if (transitionDuration.TotalMilliseconds > UInt32.MaxValue ||
+        //        transitionDuration.Ticks < 0)
+        //        throw new ArgumentOutOfRangeException("transitionDuration");
+
+        //    FrameHeader header = new FrameHeader()
+        //    {
+        //        Identifier = (uint)randomizer.Next(),
+        //        AcknowledgeRequired = true
+        //    };
+        //    UInt32 duration = (UInt32)transitionDuration.TotalMilliseconds;
+        //    var durationBytes = BitConverter.GetBytes(duration);
+        //    var b = BitConverter.GetBytes(brightness);
+
+        //    await BroadcastMessageAsync<AcknowledgementResponse>(bulb.HostName, header,
+        //        MessageType.SetLightBrightness, brightness, duration
+        //    );
+        //}
+
+        /// <summary>
+        /// Gets the current state of the bulb
+        /// </summary>
+        /// <param name="bulb"></param>
+        /// <returns></returns>
+        public Task<LightStateResponse> GetLightStateAsync(LightBulb bulb)
+		{
+			FrameHeader header = new FrameHeader()
+			{
+				Identifier = (uint)randomizer.Next(),
+				AcknowledgeRequired = false
+			};
+			return BroadcastMessageAsync<LightStateResponse>(
+				bulb.HostName, header, MessageType.LightGet);
+		}
+	}
+}
